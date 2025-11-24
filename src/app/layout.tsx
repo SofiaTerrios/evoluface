a5090ed0ef77c4af6381b78405a8a198427a3469
SofiaTerrios <sterrios1013@gmail.com>
2025-11-24 14:51:30 -0500

    fix: delete

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,11 +1,10 @@
-'use client';
+"use client";
 
-import './globals.css';
-import { Toaster } from "@/components/ui/toaster"
-import { MotionConfig } from "framer-motion"
-import VoiceControl from '@/components/VoiceControl';
-import { KnowledgeProvider } from '@/context/KnowledgeContext';
-
+import "./globals.css";
+import { Toaster } from "@/components/ui/toaster";
+import { MotionConfig } from "framer-motion";
+import VoiceControl from "@/components/VoiceControl";
+import { KnowledgeProvider } from "@/context/KnowledgeContext";
 
 export default function RootLayout({
   children,
@@ -14,19 +13,29 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="es">
-       <head>
+      <head>
         <title>EvoluFace</title>
-        <meta name="description" content="Desliza para viajar en el tiempo y ver la evolución del rostro humano." />
+        <meta
+          name="description"
+          content="Desliza para viajar en el tiempo y ver la evolución del rostro humano."
+        />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
-        <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@700&family=Open+Sans:wght@400;600&display=swap" rel="stylesheet" />
+        <link
+          rel="preconnect"
+          href="https://fonts.gstatic.com"
+          crossOrigin="anonymous"
+        />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Montserrat:wght@700&family=Open+Sans:wght@400;600&display=swap"
+          rel="stylesheet"
+        />
       </head>
       <body className="font-body antialiased">
         <KnowledgeProvider>
           <MotionConfig>
             <main>{children}</main>
           </MotionConfig>
-          <VoiceControl />
+          {/* <VoiceControl /> */}
           <Toaster />
         </KnowledgeProvider>
       </body>
